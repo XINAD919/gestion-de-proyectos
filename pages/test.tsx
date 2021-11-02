1ee5c260145e98cae205831198b91b4c7060f1db
Daniel <Da_919@hotmail.com>
2021-11-02 14:27:29 -0500

    primer commit

---
+++ pages/test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const test = () => {
+  return (
+    <div className="flex flex-col bg-gray-700">
+      <div className="">hola </div>
+      <div className="">nextjs</div>
+      <div className="">....</div>
+    </div>
+  );
+};
+
+export default test;
