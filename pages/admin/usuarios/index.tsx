8e6d0a1099e913a0449d106bcf70e460b1d3703d
Daniel <Da_919@hotmail.com>
2021-11-03 13:56:53 -0500

    se agregan titulos a las paginas y el _document

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -2,15 +2,15 @@ import Head from "next/head";
 import Link from "next/link";
 import React from "react";
 
-const test = () => {
+const index = () => {
   return (
     <>
       <Head>
-        <title>test | Gestion de proyectos</title>
+        <title>Gestion usuarios | Gestion de proyectos</title>
       </Head>
       <div className=" flex h-screen bg-gray-700">
         <div className="flex flex-col w-full items-center justify-center ">
-          <h1>Hola este es la pagina test</h1>
+          <h1>Hola este es la pagina Admin usuarios</h1>
           <Link href="/">
             <a className="cursor-pointer">
               Volver al Home <i className="fas fa-home"></i>
@@ -22,4 +22,4 @@ const test = () => {
   );
 };
 
-export default test;
+export default index;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
 export default function Home() {
   return (
-    <div className="text-2xl text-gray-800 text-center">
-      <h1>main page</h1>
-      <Link href="/test">
-        <a className="cursor-pointer">ir a test</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Home | Gestion de proyectos</title>
+      </Head>
+      <div className="text-2xl text-gray-800 text-center">
+        <h1>main page</h1>
+        <Link href="/test">
+          <a className="cursor-pointer">ir a test</a>
+        </Link>
+      </div>
+    </>
   );
 }
